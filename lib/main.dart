f972f6002f71f84145f06a4834c2ffda4810891d
Rohit Khullar <rohitkhullar786@gmail.com>
2020-08-19 19:16:08 +0200

    init

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Food Delivery',
+      title: 'Food Delivery Flutter',
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
       ),
